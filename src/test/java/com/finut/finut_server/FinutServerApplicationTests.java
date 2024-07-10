73fc3666c101b2dc961e648477957b3c7727ab84
yeojin <yeojin7010+L@gmail.com>
2024-07-10 16:53:43 +0900

    Initial commit

---
+++ src/test/java/com/finut/finut_server/FinutServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finut.finut_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinutServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
