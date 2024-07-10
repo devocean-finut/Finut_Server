d6cda975e1847b3af0c1846fa2bbaa5c65f8e80f
yeojin <yeojin7010+L@gmail.com>
2024-07-10 22:23:23 +0900

    Update HomeContorller.java : 로그인 테스트를 위한 home controller

--- src/main/java/com/finut/finut_server/controller/HomeController.java
+++ src/main/java/com/finut/finut_server/controller/HomeController.java
@@ -1,2 +1,28 @@
-package com.finut.finut_server.controller;public class HomeController {
+package com.finut.finut_server.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            model.addAttribute("username", authentication.getName());
+            System.out.println("Authentication name: " + authentication.getName());
+            System.out.println("Authorities: " + authentication.getAuthorities());
+        } else {
+            System.out.println("Authentication is null");
+        }
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
