3462361b974c1ec540cfea788c80f119439616ff
yeojin <yeojin7010+L@gmail.com>
2024-07-10 22:21:37 +0900

    Update Users.java: Users 도메인

--- src/main/java/com/finut/finut_server/domain/user/Users.java
+++ src/main/java/com/finut/finut_server/domain/user/Users.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @Entity
-public class User extends BaseTimeEntity {
+public class Users extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -31,14 +31,14 @@ public class User extends BaseTimeEntity {
     private Role role;
 
     @Builder
-    public User(String name, String email, String picture, Role role) {
+    public Users(String name, String email, String picture, Role role) {
         this.name = name;
         this.email = email;
         this.picture = picture;
         this.role = role;
     }
 
-    public User update(String name, String picture) {
+    public Users update(String name, String picture) {
         this.name = name;
         this.picture = picture;
 
