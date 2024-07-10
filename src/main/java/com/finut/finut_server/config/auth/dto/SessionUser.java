d728f0b99f2fb55caa912d13f63a2dd77fdb8c00
yeojin <yeojin7010+L@gmail.com>
2024-07-10 22:25:25 +0900

    Update SessionUser.java : 객체를 직렬화하기 위해

--- src/main/java/com/finut/finut_server/config/auth/dto/SessionUser.java
+++ src/main/java/com/finut/finut_server/config/auth/dto/SessionUser.java
@@ -1,2 +1,20 @@
-package com.finut.finut_server.config.auth.dto;public class SessionUser {
+package com.finut.finut_server.config.auth.dto;
+
+import com.finut.finut_server.domain.user.Users;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(Users user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
 }
+
