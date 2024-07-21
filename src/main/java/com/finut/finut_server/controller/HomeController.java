fb794adf3772bf774bf43f382c84f9cd5fc08b06
yeojin <yeojin7010+L@gmail.com>
2024-07-21 13:32:52 +0900

    #7 Update HomeController: HomeController swagger 태그 추가

--- src/main/java/com/finut/finut_server/controller/HomeController.java
+++ src/main/java/com/finut/finut_server/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.finut.finut_server.controller;
 
 import com.finut.finut_server.config.auth.dto.SessionUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
+@Tag(name = "Home Controller", description = "홈, 로그인, 로그아웃 관련 api")
 @Controller
 public class HomeController {
 
@@ -16,6 +19,7 @@ public class HomeController {
     public HomeController(HttpSession httpSession) {
         this.httpSession = httpSession;
     }
+    @Operation(summary = "초기 화면", description = "로그인을 위한 초기 화면을 구성합니다.")
     @GetMapping("/")
     public String home(Model model) {
         SessionUser user = (SessionUser) httpSession.getAttribute("user");
@@ -28,6 +32,7 @@ public class HomeController {
         return "home";
     }
 
+    @Operation(summary = "로그인", description = "로그인을 위한 페이지를 구성합니다.")
     @GetMapping("/login")
     public String login() {
         return "login";
