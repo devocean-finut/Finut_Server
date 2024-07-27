e9e840437b6a90ef00dec7fb5a2adb7d2c9d5c1c
plum-king <jeesoo0908@gmail.com>
2024-07-27 18:07:34 +0900

    #11 Update QuizRepository : userId를 통해 money 업데이트하는 함수 추가

--- src/main/java/com/finut/finut_server/domain/quiz/QuizRepository.java
+++ src/main/java/com/finut/finut_server/domain/quiz/QuizRepository.java
@@ -1,8 +1,10 @@
 package com.finut.finut_server.domain.quiz;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -11,4 +13,9 @@ public interface QuizRepository extends JpaRepository<Quiz, Long> {
 
     @Query("SELECT e FROM Quiz e WHERE DATE(e.createdDate) = :date")
     Quiz findByDate(@Param("date") LocalDate date);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Users u SET u.money = u.money + :moneyAmount WHERE u.id = :userId")
+    void updateMoney(Long userId, int moneyAmount);
 }
