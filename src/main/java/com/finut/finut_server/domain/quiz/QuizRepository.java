7b1642888b4747ba87a4b8570c179305abfc01ff
plum-king <jeesoo0908@gmail.com>
2024-07-27 17:19:31 +0900

    #11 Update QuizRepository : 날짜에 맞는 퀴즈 조회하는 함수 추가

--- src/main/java/com/finut/finut_server/domain/quiz/QuizRepository.java
+++ src/main/java/com/finut/finut_server/domain/quiz/QuizRepository.java
@@ -1,6 +1,14 @@
 package com.finut.finut_server.domain.quiz;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
+
+    @Query("SELECT e FROM Quiz e WHERE DATE(e.createdDate) = :date")
+    Quiz findByDate(@Param("date") LocalDate date);
 }
