c953dcc4345b741a392d1eaf259faf5a679b62db
plum-king <jeesoo0908@gmail.com>
2024-08-06 16:55:35 +0900

    #10 Update Controller : Swagger에 Response 예시 추가

--- src/main/java/com/finut/finut_server/controller/HomeController.java
+++ src/main/java/com/finut/finut_server/controller/HomeController.java
@@ -1,10 +1,14 @@
 package com.finut.finut_server.controller;
 
 import com.finut.finut_server.apiPayload.ApiResponse;
+import com.finut.finut_server.apiPayload.code.ErrorReasonDTO;
 import com.finut.finut_server.config.auth.dto.SessionUser;
 import com.finut.finut_server.domain.user.UserResponseDTO;
 import com.finut.finut_server.service.UsersService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +38,15 @@ public class HomeController {
     @Autowired
     private UsersService userService;
 
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserResponseDTO.loginUserDTO.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 에러, 관리자에게 문의 바랍니다.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorReasonDTO.class)))
+    })
     @Operation(summary = "로그인 성공", description = "로그인 성공시 해당 api에 리다이렉션 되어 수행됩니다")
     @GetMapping("/success")
     @ResponseBody
