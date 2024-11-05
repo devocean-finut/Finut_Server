2263863fd0926e57de9c3f7a6483306a2c31510e
plum-king <jeesoo0908@gmail.com>
2024-11-06 01:40:36 +0900

    #35 Update UserController : Header에서 userId 정보 조회하도록 수정

--- src/main/java/com/finut/finut_server/controller/UserController.java
+++ src/main/java/com/finut/finut_server/controller/UserController.java
@@ -2,14 +2,16 @@ package com.finut.finut_server.controller;
 
 import com.finut.finut_server.apiPayload.ApiResponse;
 import com.finut.finut_server.apiPayload.code.ErrorReasonDTO;
-import com.finut.finut_server.domain.user.UserRequestDTO;
 import com.finut.finut_server.domain.user.UserResponseDTO;
+import com.finut.finut_server.domain.user.Users;
 import com.finut.finut_server.service.UsersService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,8 +33,26 @@ public class UserController {
                             schema = @Schema(implementation = ErrorReasonDTO.class)))
     })
     @PatchMapping("/attend")
-    public ApiResponse<UserResponseDTO.updateAttendance> updateAttendance(@RequestBody UserRequestDTO.checkAttendance request){
-        UserResponseDTO.updateAttendance updateAttendance = usersService.updateAttendDate(request.getUserId());
+    public ApiResponse<UserResponseDTO.updateAttendance> updateAttendance(HttpServletRequest request, HttpServletResponse response){
+        Users user = usersService.getUserIdByToken(request, response);
+        UserResponseDTO.updateAttendance updateAttendance = usersService.updateAttendDate(user.getId());
         return ApiResponse.onSuccess(updateAttendance);
+    }
+
+    @Operation(summary = "프로필 확인", description = "사용자가 본인의 정보를 확인하는 API 입니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserResponseDTO.viewUserInfo.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "사용자 정보를 제대로 가지고 오지 못했습니다.",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 에러, 관리자에게 문의 바랍니다.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorReasonDTO.class)))
+    })
+    @GetMapping("/profile")
+    public ApiResponse<UserResponseDTO.viewUserInfo> viewUserInfo(HttpServletRequest request, HttpServletResponse response){
+        Users user = usersService.getUserIdByToken(request, response);
+        UserResponseDTO.viewUserInfo viewUserInfo = usersService.viewUserInfo(user.getId());
+        return ApiResponse.onSuccess(viewUserInfo);
     }
 }
