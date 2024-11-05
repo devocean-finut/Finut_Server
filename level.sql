53a5939fadc82a8731749c1e037fcbf376134c1e
plum-king <jeesoo0908@gmail.com>
2024-11-06 02:01:21 +0900

    #35 Update level.sql : level 테이블 내 level_name 수정

--- level.sql
+++ level.sql
@@ -1,7 +1,13 @@
-insert into finut.level(id, level, level_quiz_cnt, salary) values (1, 0, 5, 400000);
-insert into finut.level(id, level, level_quiz_cnt, salary) values (2, 1, 5, 1000000);
-insert into finut.level(id, level, level_quiz_cnt, salary) values (3, 2, 5, 2000000);
-insert into finut.level(id, level, level_quiz_cnt, salary) values (4, 3, 5, 4000000);
-insert into finut.level(id, level, level_quiz_cnt, salary) values (5, 4, 5, 6000000);
-insert into finut.level(id, level, level_quiz_cnt, salary) values (6, 5, 5, 8000000);
-insert into finut.level(id, level, level_quiz_cnt, salary) values (7, 6, 5, 10000000);
+/*
+-- Query: SELECT * FROM finut.level
+LIMIT 0, 5000
+
+-- Date: 2024-11-06 01:59
+*/
+INSERT INTO `` (`id`,`created_date`,`modified_date`,`level_quiz_cnt`,`salary`,`level_name`) VALUES (1,NULL,NULL,5,400000,'PARTTIMEJOB');
+INSERT INTO `` (`id`,`created_date`,`modified_date`,`level_quiz_cnt`,`salary`,`level_name`) VALUES (2,NULL,NULL,5,1000000,'INTERN');
+INSERT INTO `` (`id`,`created_date`,`modified_date`,`level_quiz_cnt`,`salary`,`level_name`) VALUES (3,NULL,NULL,5,2000000,'STAFF');
+INSERT INTO `` (`id`,`created_date`,`modified_date`,`level_quiz_cnt`,`salary`,`level_name`) VALUES (4,NULL,NULL,5,4000000,'ASSOCIATEMANAGER');
+INSERT INTO `` (`id`,`created_date`,`modified_date`,`level_quiz_cnt`,`salary`,`level_name`) VALUES (5,NULL,NULL,5,6000000,'MANAGER');
+INSERT INTO `` (`id`,`created_date`,`modified_date`,`level_quiz_cnt`,`salary`,`level_name`) VALUES (6,NULL,NULL,5,8000000,'SENIORMANAGER');
+INSERT INTO `` (`id`,`created_date`,`modified_date`,`level_quiz_cnt`,`salary`,`level_name`) VALUES (7,NULL,NULL,5,10000000,'DIRECTOR');
