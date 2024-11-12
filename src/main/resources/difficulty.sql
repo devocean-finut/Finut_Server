f1d7733ba54307436341ac009dda0bc9418cc6ac
yeojin <yeojin7010+L@gmail.com>
2024-11-12 22:50:48 +0900

    Update CI

--- src/main/resources/difficulty.sql
+++ src/main/resources/difficulty.sql
@@ -1,3 +1,7 @@
-INSERT INTO Difficulty (difficulty, diff_quiz_cnt) VALUES ('MI', 3);
-INSERT INTO Difficulty (difficulty, diff_quiz_cnt) VALUES ('HI', 3);
-INSERT INTO Difficulty (difficulty, diff_quiz_cnt) VALUES ('LO', 3);
+use finut;
+
+INSERT INTO difficulty (difficulty, diff_quiz_cnt) VALUES ('MI', 3);
+INSERT INTO difficulty (difficulty, diff_quiz_cnt) VALUES ('HI', 3);
+INSERT INTO difficulty (difficulty, diff_quiz_cnt) VALUES ('LO', 3);
+
+select * from difficulty;
