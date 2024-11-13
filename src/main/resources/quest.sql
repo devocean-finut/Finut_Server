a1f11ac1bda3ca98f387f5e9912080bf398c724e
yeojin <yeojin7010+L@gmail.com>
2024-11-13 20:59:04 +0900

    Update Poduct: product sql 추가

--- src/main/resources/quest.sql
+++ src/main/resources/quest.sql
@@ -1,10 +1,10 @@
 use finut;
 
-INSERT INTO quest (next_level) VALUES ('INTERN');
-INSERT INTO quest (next_level) VALUES ('STAFF');
-INSERT INTO quest (next_level) VALUES ('ASSOCIATEMANAGER');
-INSERT INTO quest (next_level) VALUES ('MANAGER');
-INSERT INTO quest (next_level) VALUES ('SENIORMANAGER');
-INSERT INTO quest (next_level) VALUES ('DIRECTOR');
+INSERT INTO quest (id, next_level) VALUES (1,'INTERN');
+INSERT INTO quest (id, next_level) VALUES (2,'STAFF');
+INSERT INTO quest (id, next_level) VALUES (3,'ASSOCIATEMANAGER');
+INSERT INTO quest (id, next_level) VALUES (4,'MANAGER');
+INSERT INTO quest (id, next_level) VALUES (5,'SENIORMANAGER');
+INSERT INTO quest (id, next_level) VALUES (6,'DIRECTOR');
 
 select * from quest;
