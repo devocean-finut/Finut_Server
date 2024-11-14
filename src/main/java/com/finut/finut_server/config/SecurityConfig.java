b5b062407900aee492eab6e65b7ae03def66751a
plum-king <jeesoo0908@gmail.com>
2024-11-14 23:52:02 +0900

    #39 Add Cors Setting : Cors 세팅 추가

--- src/main/java/com/finut/finut_server/config/SecurityConfig.java
+++ src/main/java/com/finut/finut_server/config/SecurityConfig.java
@@ -18,6 +18,11 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -47,6 +52,7 @@ public class SecurityConfig {
                                 .logoutSuccessUrl("/") // 임시
                 )
                 .csrf(AbstractHttpConfigurer::disable) // post 요청을 위한 csrf disable
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
         return http.build();
     }
@@ -71,5 +77,18 @@ public class SecurityConfig {
         CustomOAuth2UserService customOAuth2UserService = new CustomOAuth2UserService(userRepository, httpSession);
         customOAuth2UserService.setAuthorizedClientService(authorizedClientService);
         return customOAuth2UserService;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 프론트엔드 도메인
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")); // 허용할 HTTP 메서드
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // 허용할 헤더
+        configuration.setExposedHeaders(Arrays.asList("Authorization")); // 응답에서 노출할 헤더
+        configuration.setAllowCredentials(true); // 자격 증명 포함 요청 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+        return source;
     }
 }
